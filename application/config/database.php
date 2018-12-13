1599664a042657728e9f02ebfb9d20e9c61ce49e
Harshal <harshal@spotsoon.com>
2018-12-13 18:27:29 +0530

    updated database file

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'root',
+	'hostname' => 'srinivasadb.cozidat4hemk.ap-south-1.rds.amazonaws.com',
+	'username' => 'srinivasa',
+	'password' => 'srinivasa_123',
 	'database' => 'srinivasa',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
