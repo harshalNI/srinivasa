07065b320eba625a602e3cdc495e0cc3d3b231c3
Harshal <harshal@spotsoon.com>
2018-12-13 18:37:46 +0530

    updated database file

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'srinivasa',
 	'password' => 'srinivasa_123',
 	'database' => 'srinivasa',
-	'dbdriver' => 'mysql',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
